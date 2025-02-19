04c9b9d7bbc3d11fa14b83e125abbcfc911ca59a
fatima <fatimaejaz2k18@gmail.com>
2025-02-19 13:18:50 +0530

    Vehicle assignment completed

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -23,10 +23,24 @@ public class Car extends Vehicle {
     }
 
     public void changeGear(int newGear){
+
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+
+
+        if (newGear > 0 && newGear <= gears) {
+            this.currentGear = newGear;
+            System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+        } else {
+            System.out.println("Invalid gear: " + newGear);
+        }
+
     }
     public void changeSpeed(int newSpeed, int newDirection){
-
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
-}
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    }
+
